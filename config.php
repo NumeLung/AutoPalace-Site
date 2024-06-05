afb26b0148f06b1b11efa02ecb1b4a2b3ff89621
Ivaylo Kolev <120529438+NumeLung@users.noreply.github.com>
2024-06-05 17:32:44 +0300

    trying to pull of the ajax methods

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+const HOST = "127.0.0.1";
+const DBUSER = "root";
+const DBPASS = "";
+const DBNAME = "torquegt";
+$CONN = new mysqli(HOST, DBUSER, DBPASS, DBNAME);
--- db_info.php
+++ db_info.php
@@ -2,5 +2,5 @@
 const HOST = "127.0.0.1";
 const DBUSER = "root";
 const DBPASS = "";
-const DBNAME = "test";
+const DBNAME = "toqruegt";
 $CONN = new mysqli(HOST, DBUSER, DBPASS, DBNAME);
